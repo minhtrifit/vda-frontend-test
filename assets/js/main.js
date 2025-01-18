072e7c4687c2a7e14c07b5dabe3d0ec72cdfc8c9
minhtrifit <minhtri.fit@gmail.com>
2025-01-18 23:27:33 +0700

    feat: product-category-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,48 @@
-document.querySelector(".navbar-logo").addEventListener("click", function () {
+// ============================== Navbar logo ==============================
+const navbarLogo = document.querySelector(".navbar-logo");
+
+navbarLogo.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// ============================== Flash sale slider ==============================
+const flashSaleSwiper = new Swiper(".swiper.flash-sale-swiper", {
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 10,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+// ============================== Product category slider ==============================
+var swiper = new Swiper(".product-category-swiper", {
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 10,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
